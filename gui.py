f742ddd05c8bbf76fe9f138b1bef2c0a60671de6
JueK3y <54510690+JueK3y@users.noreply.github.com>
2021-01-12 21:38:13 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
+import sys
+import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
+
 
 def show_data():
     print(str(e1.get()))
@@ -6,67 +10,62 @@ def show_data():
     print(str(e3.get()))
     print(str(e4.get()))
 
+def exit():
+    messagebox.showerror("Program stopped", "The process was cancelled by the user!")
+    sys.exit(0)
 
-window = Tk()
+root = tk.Tk()
+root.wm_attributes("-topmost", 1)
 
 # Label
-li = Label(window, text="Title")
+li = Label(root, text="IG URL")
 li.grid(row=0, column=0)
 
-li = Label(window, text="Author")
+li = Label(root, text="Username")
 li.grid(row=0, column=2)
 
-li = Label(window, text="Year")
+li = Label(root, text="Browser")
 li.grid(row=1, column=0)
 
-li = Label(window, text="ISBN")
+li = Label(root, text="Password")
 li.grid(row=1, column=2)
 
 # Input
-title_text = StringVar()
-e1 = Entry(window, textvariable=title_text)
+url_text = StringVar()
+e1 = Entry(root, textvariable=url_text)
 e1.grid(row=0, column=1)
 
-author_text = StringVar()
-e2 = Entry(window, textvariable=author_text)
+username_text = StringVar()
+e2 = Entry(root, textvariable=username_text)
 e2.grid(row=0, column=3)
 
-year_text = StringVar()
-e3 = Entry(window, textvariable=year_text)
+browser_text = StringVar()
+e3 = Entry(root, textvariable=browser_text)
 e3.grid(row=1, column=1)
 
-isbn_text = StringVar()
-e4 = Entry(window, textvariable=isbn_text)
+password_text = StringVar()
+e4 = Entry(root, textvariable=password_text, show='*')
 e4.grid(row=1, column=3)
 
 #
-list1 = Listbox(window, height=6, width=35)
+list1 = Listbox(root, height=6, width=35)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 #
-sb1 = Scrollbar(window)
+sb1 = Scrollbar(root)
 sb1.grid(row=2, column=2, rowspan=6)
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
 # Buttons
-b1 = Button(window, text="View All", width=12, command=show_data)
+b1 = Button(root, text="Run", width=12, command=show_data)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search Entry", width=12)
+b2 = Button(root, text="Help", width=12)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add Entry", width=12)
-b3.grid(row=4, column=3)
+b6 = Button(root, text="Exit", width=12, command=exit)
+b6.grid(row=4, column=3)
 
-b4 = Button(window, text="Update selected", width=12)
-b4.grid(row=5, column=3)
-
-b5 = Button(window, text="Delete selected", width=12)
-b5.grid(row=6, column=3)
-
-b6 = Button(window, text="Close", width=12)
-b6.grid(row=6, column=3)
-
-window.mainloop()
+root.mainloop()
