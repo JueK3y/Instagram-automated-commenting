a42c9105a7bbf8780d76ffcc8460e2b1f09781ee
JueK3y | Personal Account <54510690+JueK3y-Prv@users.noreply.github.com>
2021-07-11 16:50:59 +0200

    Add files via upload

---
+++ REWORK/main.py
@@ -0,0 +1,13 @@
+'''
+DO NOT indicate used program sections as your own.
+DO NOT sell the software to other people under your name.
+Before further development and sale, a written agreement must be made with the manufacturer (JueK3y).
+In the event of possible damage, the user alone is liable, the manufacturer (JueK3y) withdraws from any legal responsibility.
+Copyright © 2020 - 2021 by JueK3y (www.github.com/JueK3y/)
+'''
+
+import sys
+
+print("This is the start of IAC 2.0.")
+
+sys.stdout.flush()
