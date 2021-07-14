e8c839ac2fe8c3ca6581f9a40defefae17d451e3
Julian Kennedy <julian.kennedy@esg.de>
2021-07-14 13:48:29 +0200

    Updated content

--- REWORK/src/js/index.js
+++ REWORK/src/js/index.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
-    $('.burger').click(function() {
-        $('.nav-content').toggleClass('active')
-        $('.burger').toggleClass('toggle')
-    })
-})
+$(document).ready(function(){
+    $("#burger-input").click(function(){
+        if($(this).is(":checked")){
+            $("#side-bar").animate({left: '15px', opacity: '1'});
+            $("#big-line").animate({left: '60px', opacity: '1'}, 'slow');
+        }
+        else if($(this).is(":not(:checked)")){
+            $("#side-bar").animate({left: '-60px', opacity: '0'});
+            $("#big-line").animate({left: '0px', opacity: '0'}, 'slow');
+        }
+    });
+});
