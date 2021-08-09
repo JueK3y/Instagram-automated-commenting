68147a81b1cc9439809ccf95d2f6cc267b36b1b1
Julian Kennedy <julian.kennedy@esg.de>
2021-08-09 15:33:48 +0200

    Updated wifi icons

--- REWORK/src/js/index.js
+++ REWORK/src/js/index.js
@@ -6,12 +6,18 @@ $(document).ready(function() {
         $("main").css('width', '100vw').css('width', '-=35px')
         $("main").animate({left: '45px'})
         setTimeout(function() {
+          $("#side-bar").css('width', '55px')
+        }, 300)
+        setTimeout(function() {
           $(".navText").css('display', 'none');
         }, 500)
         slideIn = true
       }
       else if (slideIn) {
-        $(".navText").css('display', 'block');
+        $("#side-bar").css('width', '320px')
+        setTimeout(function() {
+          $(".navText").css('display', 'block');
+        }, 50)
         $("main").animate({left: '312px'})
         setTimeout(function() {
           $("main").css('width', '100vw').css('width', '-=312px')
@@ -24,17 +30,21 @@ $(document).ready(function() {
 
 var elem = document.getElementById("changeText")
 var changeColor = document.getElementById("wifi")
+var changeImg = document.getElementById("wifi-img")
 
 window.setInterval(function() {
   var text = Math.floor(Math.random() * 25)
   if (text <= 5) {
-    changeColor.style.color = "red"
+    changeColor.style.background = "#FDE7E9"
+    changeImg.src = "/src/img/icons/wifi/wifi-bad.svg"
   }
   else if (text <= 15) {
-    changeColor.style.color = "orange"
+    changeColor.style.background = "#FFF4CE"
+    changeImg.src = "/src/img/icons/wifi/wifi-okay.svg"
   }
   else if (text > 15) {
-    changeColor.style.color = "green"
+    changeColor.style.background = "none"
+    changeImg.src = "/src/img/icons/wifi/wifi-good.svg"
   }
   elem.innerHTML = text;
 }, 2000);
