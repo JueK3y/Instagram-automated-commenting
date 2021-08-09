a20a8d6cbf210ead7503895cc26ac4342e722d8a
Julian Kennedy <julian.kennedy@esg.de>
2021-08-09 14:11:22 +0200

    Updated icons, animation and ui

--- REWORK/src/js/index.js
+++ REWORK/src/js/index.js
@@ -1,24 +1,40 @@
-$(document).ready(function(){
-    $(".active").click(function(){
-        alert("Minimize navbar!")
-    });
-});
+slideIn = false
+ 
+$(document).ready(function() {
+    $("#active").click(function() {
+      if (slideIn == false) {
+        $("main").css('width', '100vw').css('width', '-=35px')
+        $("main").animate({left: '45px'})
+        setTimeout(function() {
+          $(".navText").css('display', 'none');
+        }, 500)
+        slideIn = true
+      }
+      else if (slideIn) {
+        $(".navText").css('display', 'block');
+        $("main").animate({left: '312px'})
+        setTimeout(function() {
+          $("main").css('width', '100vw').css('width', '-=312px')
+        }, 400)
+        slideIn = false
+      }
+    })
+})
 
-function togglePassword() {
-    const x = document.getElementById("password-form");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+
+var elem = document.getElementById("changeText")
+var changeColor = document.getElementById("wifi")
+
+window.setInterval(function() {
+  var text = Math.floor(Math.random() * 25)
+  if (text <= 5) {
+    changeColor.style.color = "red"
   }
-
-
-/* if($(this).is(":checked")){
-    $("#side-bar").animate({left: '0px', opacity: '1'});
-    $("#big-line").animate({left: '60px', opacity: '1'}, 'slow');
-}
-else if($(this).is(":not(:checked)")){
-    $("#side-bar").animate({left: '-80px', opacity: '0'});
-    $("#big-line").animate({left: '0px', opacity: '0'}, 'slow');
-} */
+  else if (text <= 15) {
+    changeColor.style.color = "orange"
+  }
+  else if (text > 15) {
+    changeColor.style.color = "green"
+  }
+  elem.innerHTML = text;
+}, 2000);
