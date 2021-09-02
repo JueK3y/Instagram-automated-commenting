fd0184c283505acc5010e69b6e5aaa50e79da9cb
Julian Kennedy <julian.kennedy@esg.de>
2021-09-02 11:31:26 +0200

    Updated nav animation

--- REWORK/src/js/index.js
+++ REWORK/src/js/index.js
@@ -2,18 +2,25 @@
 
 $('.profile').click(() => {
   if (! $('#active').hasClass('profile')) {
-    $('.line').animate({height: '55px', marginBottom: '-55px', opacity: '0.7'}, 250)
-    $('#active').removeAttr('id')
-    $('.profile').attr('id', 'active')
-    $('.line').css('margin-top', '-38px')
-    $('.line').animate({marginTop: '1.5px', height: '16px', opacity: '1'}, 200)
+    $('.line').animate({height: '55px', marginBottom: '-55px', opacity: '0.7'}, 200)
+    setTimeout(() => {
+      $('#active').removeAttr('id')
+      $('.profile').attr('id', 'active')
+      $('.line').css('margin-top', '-38px')
+      $('.line').animate({marginTop: '1.5px', height: '16px', opacity: '1'}, 200)
+    }, 1)
   }
 }) 
 
-$('.comment').click(() => {                         // Needs to be animated too
+$('.comment').click(() => {
   if (! $('#active').hasClass('comment')) {
-    $('#active').removeAttr('id')
-    $('.comment').attr('id', 'active')
+    $('.line').animate({marginTop: '-38px', height: '55px', opacity: '0.7'}, 200)
+    setTimeout(() => {
+      $('#active').removeAttr('id')
+      $('.comment').attr('id', 'active')
+      $('.line').css('margin-top', '1.5px')   
+      $('.line').animate({height: '16px', opacity: '1'}, 200)
+    }, 230)
   }
 }) 
 
