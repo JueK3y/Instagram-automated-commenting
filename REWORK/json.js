2d1af6ac2ebe07480009244cabddd45713a5ed87
JueK3y <no@mail.com>
2021-12-28 14:40:45 +0100

    Updated JSON creation

--- REWORK/json.js
+++ REWORK/json.js
@@ -1,14 +1,49 @@
 const Store = require('electron-store')
 
-const save = new Store()
+var today = new Date()
+const store = new Store()
 
-save.set('color', {
-    name: 'JSON Name',
-    code: {
-        rgb: [0, 1, 2],
-        hex: '#test'
-    }
-})
+// Create new user with the getSaveState
+function createUser(username) {
+    store.set({
+        uID: {
+            "username": username,
+            "nickname": username,
+            "userID": "Test-User-ID",
+            "pinned": false,
+            "created": {
+                "date": today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear(),
+                "time": today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            },
+            "verified": {
+                "checked": false,
+                "working": false,
+            }
+        }
+    })
+    // console.log(store.get('uID.created.time'))
+}
 
-save.get('color.name')
-console.log(save.get('color.name'))
+// Update user data
+function updateUser(username, nickname, pinned) {
+    if (nickname == null) nickname = username
+    if (pinned == null) pinned = false
+
+    store.set({
+        uID: {
+            "username": username,
+            "nickname": nickname,
+            "userID": "Test-User-ID",
+            "pinned": pinned,
+            "created": {
+                "date": today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear(),
+                "time": today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            },
+            "verified": {
+                "checked": false,
+                "working": false,
+            }
+        }
+    })
+    // console.log(store.get('uID.nickname'))
+}
