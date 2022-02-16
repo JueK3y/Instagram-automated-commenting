a135517defd95799f46041dcdb958e2b6566e454
JueK3y <juek3y@gmail.com>
2022-02-16 13:17:29 +0100

    Updated main function

--- REWORK/main.js
+++ REWORK/main.js
@@ -161,17 +161,41 @@ app.on('activate', () => {
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
 
 
-const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://instagram.com');
-  await page.screenshot({ path: 'Instagram.png' });
+const puppeteer = require('puppeteer-extra')
 
-  await browser.close();
-})();
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+
+puppeteer.launch({ headless: false, slowMo: 100 }).then(async browser => {    // Without {} args in production
+  const page = await browser.newPage()
+  
+  // Stealh mode checker
+  /*await page.goto('https://arh.antoinevastel.com/bots/areyouheadless')
+  await page.waitForTimeout(5000)
+  await page.screenshot({ path: 'Stealth.png' })*/
+  
+  const username = 'username'
+  const password = 'password'
+  const postURL = 'https://www.instagram.com/accounts/login/'
+
+  await page.goto(postURL);
+
+  // Waiting for LogIn load
+  await page.waitForSelector('input[name="username"]')
+
+  // Entering LogIn data
+  await page.type('input[name="username"]', username)
+  await page.type('input[name="password"]', password)
+
+  // Submit LogIn data
+  page.click('[type="submit"]'),
+  page.waitForNavigation({ waitUntil: 'networkidle0' })
+
+  await page.screenshot({ path: 'Instagram.png' })
+  
+  // await browser.close();
+})
