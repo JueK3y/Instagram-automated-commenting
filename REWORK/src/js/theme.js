1abef1e014e4f71678ca776bab17c18c1ca36071
Julian Kennedy <juek3y@gmail.com>
2022-02-22 13:03:32 +0100

    Updated array

--- REWORK/src/js/theme.js
+++ REWORK/src/js/theme.js
@@ -15,7 +15,7 @@ const hex2rgb = (hex) => {
     const r = parseInt(hex.slice(1, 3), 16)
     const g = parseInt(hex.slice(3, 5), 16)
     const b = parseInt(hex.slice(5, 7), 16)
-    return [ r, g, b ]
+    return [r, g, b]
 }
 
 let color;
